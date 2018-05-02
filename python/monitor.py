467e1298eccffe7c6dcb9bca1b809d1889bdf742
fd-rey <franciscodomingo.rey01@estudiant.upf.edu>
2018-05-02 15:47:02 +0200

    added sample number to the output

--- python/monitor.py
+++ python/monitor.py
@@ -7,37 +7,41 @@ def cpuDump(shouldStop):
     filename ="/tmp/cpu_dump.txt"
     f = open(filename,"a")
     now = time.time()
-    f.write('### '+str(datetime.datetime.now()).split('.')[0]+"\n")
+    f.write('# '+str(datetime.datetime.now()).split('.')[0]+"\n")
     print('running cpuDump')
+    sample = 0.0
     while True:
         if shouldStop.value == 1:
             break;
 
         data = psutil.cpu_percent(interval=0.5, percpu=True)
-        f.write(str(data)+"\n")
-        print(str(data)+"\n")
+        f.write(str(data)+" "+str(sample)+" \n")
+        print(str(data)+" "+str(sample))
+        sample += 0.5;
     f.close()
 
 
 def memDump(shouldStop):
     f = open("/tmp/mem_dump.txt","a")
-    f.write('### '+str(datetime.datetime.now()).split('.')[0]+"\n")
+    f.write('# '+str(datetime.datetime.now()).split('.')[0]+"\n")
     mem = psutil.virtual_memory()
     totalInMibs = mem.total/float(1024*1024)
-    f.write("Total memory: %.2f MiB\n" % (totalInMibs))
+    f.write("# Total memory: %.2f MiB\n" % (totalInMibs))
     print('running memDump')
+    sample = 0.0
     while True:
         if shouldStop.value == 1:
             break;
         data =  psutil.virtual_memory()
-        f.write(str(data.percent)+"\n")
-        print(str(data.percent))
+        f.write(str(data.percent)+" "+str(sample)+" \n")
+        print(str(data.percent)+" "+str(sample))
+        sample += 0.5;
         time.sleep(0.5)
     f.close()
 
 def consumeResources():
     #create a large matrix
-    M = 15000
+    M = 19000
     A = []
     for i in range (M):
         A.append([0 for x in range(M)])
