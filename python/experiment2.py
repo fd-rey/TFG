96a4cb7f0afe0a4860ad61668850ad5227416968
fd-rey <franciscodomingo.rey01@estudiant.upf.edu>
2018-06-05 17:04:07 -0700

    experiment2: parse status.conditions array of the get pod output

--- python/experiment2.py
+++ python/experiment2.py
@@ -3,6 +3,12 @@ import json
 
 if __name__ == '__main__':
     filename ="/tmp/test.json"
-    f = open(filename,"a")
-    result = subprocess.check_output(['kubectl','get','pods','-o','json'])
+    f = open(filename,"w")
+    result = subprocess.check_output(['kubectl','get','pods','-o','json']).rstrip()
     f.write(result)
+    f.close()
+    #
+    f = open(filename,"r")
+    data = json.load(f)
+    #print(data)
+    print data['items'][0]['status']['conditions']
